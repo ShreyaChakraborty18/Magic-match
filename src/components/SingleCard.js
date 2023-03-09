3bab2ec3ef282a9f35cbfdf32f65212a69af80ff
Shreya Chakraborty <schakraborty1899@gmail.com>
2023-03-09 13:58:20 +0530

    initial commit

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,25 +1,3 @@
-// import './SingleCard.css';
-
-
-// export default function SingleCard({ card, handleChoice, flipped }) {
-
-//     const handleClick = () => {
-//         handleChoice(card)
-//     }
-//     return (
-//         <div className='card' >
-//         <div className={flipped ? "flipped" : ""}>
-//           <img className="front" src={card.src} alt="card front" />
-//           <img className="back" 
-//                src="/img/cover-2.png" 
-//                onClick={handleClick}
-//                alt="card back"
-//             />
-//         </div>
-//       </div>
-//     )
-// }
-
 import './SingleCard.css'
 
 export default function SingleCard({ card, handleChoice, flipped, disabled }) {
